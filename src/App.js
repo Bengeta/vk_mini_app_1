925a7bdfa4ee67a616d068090f401ff610005fdf
Bengeta <inovozhilov08@gmail.com>
2021-09-06 21:34:18 +0300

    попробовал сделать ффонарик

--- src/App.js
+++ src/App.js
@@ -1,46 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
+import {View, ScreenSpinner, AdaptivityProvider, AppRoot} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
 
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {lightOn: false};
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+        this.setLightOn = this.setLightOn.bind(this);
+        this.turnOnLight = this.turnOnLight.bind(this);
+    }
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+    numbers = {"0": false, "1": false, "2": false, "3": false, "4": false, "5": false, "6": false, "7": false}
 
-	return (
-		<AdaptivityProvider>
-			<AppRoot>
-				<View activePanel={activePanel} popout={popout}>
-					<Home id='home' fetchedUser={fetchedUser} go={go} />
-					<Persik id='persik' go={go} />
-				</View>
-			</AppRoot>
-		</AdaptivityProvider>
-	);
+    isUsed = false;
+
+    setLightOn(id) {
+        this.numbers[id] = !this.numbers[id];
+    }
+
+    turnOnLight() {
+        while (this.isUsed)
+            for (let i = 0; i < this.numbers.length; i++) {
+                if (this.numbers[i]) {
+                    bridge.send("VKWebAppFlashSetLevel", {"level": 1});
+                    setTimeout(() => {
+                    }, 1000);
+                } else {
+                    bridge.send("VKWebAppFlashSetLevel", {"level": 0});
+                    setTimeout(() => {
+                    }, 1000);
+                }
+                i %= this.numbers.length;
+            }
+    }
+
+    render() {
+        return (
+            <div>
+                <NumberList numbers={this.numbers} setLightOn={this.setLightOn()}/>
+                <button onClick={this.setLightOn}>
+                    {this.state.turnOn ? "on" : "off"}
+                </button>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {turnOn: props.turnOn};
+        this.id = this.props.id
+        this.setLightOn = this.setLightOn.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setLightOn(event) {
+        this.props.setLightOn({id});
+        this.setState(prevState => ({
+            turnOn: !prevState.turnOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.setLightOn}>
+                {this.state.turnOn ? "on" : "off"}
+            </button>
+        );
+    }
+}
+
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <Button id={number.id.toString()} setLightOn={props.setLightOn}/>
+    );
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    );
 }
 
 export default App;
